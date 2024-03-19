f26c4ce033fe76180afe044e3ab85f55ea4c2d66
Alex <alex.f.wulf@gmail.com>
2024-03-19 22:25:00 +0100

    Player tested

---
+++ tests/Unit/Core/Domain/Player/PlayerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit\Core\Domain\Player;
+
+use Core\Domain\Player\Player;
+use Core\Domain\Player\PlayerId;
+use Core\Domain\Player\PlayerTypeEnum;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(Player::class)]
+class PlayerTest extends TestCase
+{
+    public function test_equals_returns_true(): void
+    {
+        $player = new Player(PlayerId::make(), 'GAME_ID', PlayerTypeEnum::INVITED);
+        $otherPlayer = $player;
+
+        $this->assertTrue($player->equals($otherPlayer));
+    }
+
+    public function test_equals_returns_false(): void
+    {
+        $playerId = PlayerId::make();
+        $player = new Player($playerId, 'GAME_ID', PlayerTypeEnum::INVITED);
+
+        $otherPlayer = new Player(PlayerId::make(), 'GAME_ID', PlayerTypeEnum::INVITED);
+        $this->assertFalse($player->equals($otherPlayer));
+
+        $otherPlayer = new Player($playerId, 'GAME_ID2', PlayerTypeEnum::INVITED);
+        $this->assertFalse($player->equals($otherPlayer));
+
+        $otherPlayer = new Player($playerId, 'GAME_ID', PlayerTypeEnum::INVITING);
+        $this->assertFalse($player->equals($otherPlayer));
+    }
+
+
+}
